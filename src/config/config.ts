b7c720637d4defb83988a4b5d488f96b025e28b3
zerocool <phichaya6310@gmail.com>
2023-07-09 03:59:19 +0700

    user and config

---
+++ src/config/config.ts
@@ -0,0 +1,17 @@
+export const config = () => ({
+    PORT: process.env.PORT,
+    APP_NAME: process.env.APP_NAME,
+    JWT_SECRET: process.env.JWT_SECRET,
+  
+    MONGODB_URI: `mongodb${process.env.DB_PROTOCOL}://${
+      process.env.DB_USERNAME
+    }:${process.env.DB_PASSWORD}@${process.env.DB_HOST}${
+      process.env.DB_PORT ? ':' + process.env.DB_PORT : ''
+    }/${process.env.DB_DATABASE}`,
+  
+    MONGODB_URI_LOCAL: `mongodb://${process.env.DB_HOST}${
+      process.env.DB_PORT ? ':' + process.env.DB_PORT : ''
+    }/${process.env.DB_DATABASE}`,
+  
+  });
+  
