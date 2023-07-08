d48be2c0954cba44f243f21dbadd9f05f5272871
zerocool <phichaya6310@gmail.com>
2023-07-09 04:00:15 +0700

    authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+    ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string' },
+        password: { type: 'string' },
+      },
+    },
+  })
+  @Post('authentication/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
 
   @Get()
   getHello(): string {
